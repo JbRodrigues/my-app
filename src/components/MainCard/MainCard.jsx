4a0e50f8038098296c5304207dea1d6d47675285
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2022-06-29 10:48:42 -0300

    Add category and news description on the main card

--- src/components/MainCard/MainCard.jsx
+++ src/components/MainCard/MainCard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import './MainCard.css'
 
 function MainCard() {
     return (
         <div className="MainCard">
-
+            <div className="category">Política</div>
+            <div className="news-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, dolor nihil id, blanditiis fugiat assumenda quod ut dignissimos voluptatem numquam magni voluptatum eum illo iste deserunt fuga recusandae voluptates ducimus!</div>
         </div>
     );
 }
